573d935fd534d7484233da5a2f5978d0eed74319
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-10-12 00:02:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//высокий уровень вариант 13
+Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y= double.Parse(Console.ReadLine());
+Console.WriteLine((x>=-1&& x<=0&&y<=x+1&&y<=-x+1&&y<=1&&y>=0&&x>=0&&x<=1));
+
+
